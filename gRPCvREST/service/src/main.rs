7dcdbd032eb852b861bc6db74f86f84c56a5f03d
Nathan Prescott <echodog@gmail.com>
2023-08-14 09:49:12 -0700

    Updated image request to load images on startup and return the different qualities when called

--- gRPCvREST/service/src/main.rs
+++ gRPCvREST/service/src/main.rs
@@ -1,10 +1,8 @@
 use actix_web::web::Data;
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use image::error::UnsupportedErrorKind::Color;
-use image::{ColorType, DynamicImage, ExtendedColorType};
-use jpeg_encoder::{Encoder, JpegColorType};
+use image::DynamicImage;
+use jpeg_encoder::Encoder;
 use serde::Deserialize;
-use std::io::BufRead;
 use std::thread;
 use std::time::Instant;
 
@@ -13,72 +11,92 @@ enum Size {
     Small,
     Medium,
     Large,
+    ExtraLarge,
 }
 
 struct Images {
-    small: DynamicImage,
-    medium: DynamicImage,
-    large: DynamicImage,
+    small: Vec<u8>,
+    medium: Vec<u8>,
+    large: Vec<u8>,
+    extra_large: Vec<u8>,
 }
 
 impl Images {
     fn new() -> &'static Images {
         let start = Instant::now();
 
-        let paths = vec![
-            "src/files/image_small.JPG",
-            "src/files/image_medium.JPG",
-            "src/files/image_large.JPG",
-        ];
+        let path = "src/files/image.JPG";
+        let image = image::open(path).unwrap_or_default();
 
-        let images = thread::spawn(move || {
-            let mut images = Vec::new();
-            for path in paths {
-                images.push(image::open(path).unwrap_or_default());
+        let sizes = vec![Vec::new(), Vec::new(), Vec::new(), Vec::new()];
+
+        let mut handles = Vec::new();
+        let mut encoder_quality = 25;
+        for mut size in sizes {
+            let image = image.clone();
+
+            let handle = thread::spawn(move || {
+                println!("Encoding image {:?}...", &mut size);
+                Self::image_encode(image, &mut size, encoder_quality);
+                size
+            });
+
+            encoder_quality += 25;
+
+            if encoder_quality > 75 {
+                encoder_quality = 90;
             }
-            images
-        })
-        .join()
-        .unwrap();
+
+            handles.push(handle);
+        }
+
+        let mut results = Vec::new();
+        for handle in handles {
+            results.push(handle.join().unwrap());
+        }
+
+        let (small, medium, large, extra_large) = (
+            results.get(0).unwrap(),
+            results.get(1).unwrap(),
+            results.get(2).unwrap(),
+            results.get(3).unwrap(),
+        );
 
         let duration = start.elapsed();
         println!("Images loaded in: {:?}", duration);
 
         Box::leak(Box::new(Images {
-            small: images[0].clone(),
-            medium: images[1].clone(),
-            large: images[2].clone(),
+            small: small.clone(),
+            medium: medium.clone(),
+            large: large.clone(),
+            extra_large: extra_large.clone(),
         }))
+    }
+
+    fn image_encode(image: DynamicImage, mut size: &mut Vec<u8>, quality: u8) {
+        let encoder = Encoder::new(&mut size, quality);
+
+        encoder
+            .encode(
+                image.as_bytes(),
+                image.width() as u16,
+                image.height() as u16,
+                jpeg_encoder::ColorType::Rgb,
+            )
+            .unwrap();
     }
 }
 
 #[get("/image/request/{size}")]
 async fn image_request(size: web::Path<Size>, data: Data<&Images>) -> impl Responder {
-    let mut output = Vec::new();
-    let encoder = Encoder::new(&mut output, 96);
-
-    encoder
-        .encode(
-            data.small.as_bytes(),
-            data.small.width() as u16,
-            data.small.height() as u16,
-            jpeg_encoder::ColorType::Rgb,
-        )
-        .unwrap();
+    let output = match size.into_inner() {
+        Size::Small => data.small.clone(),
+        Size::Medium => data.medium.clone(),
+        Size::Large => data.large.clone(),
+        Size::ExtraLarge => data.extra_large.clone(),
+    };
 
     HttpResponse::Ok().content_type("image/jpeg").body(output)
-
-    // match size.into_inner() {
-    //     Size::Small => HttpResponse::Ok()
-    //         .content_type("image/jpeg")
-    //         .body(data.small.as_bytes().to_vec()),
-    //     Size::Medium => HttpResponse::Ok()
-    //         .content_type("image/jpeg")
-    //         .body(data.medium.as_bytes().to_vec()),
-    //     Size::Large => HttpResponse::Ok()
-    //         .content_type("image/jpeg")
-    //         .body(data.large.as_bytes().to_vec()),
-    // }
 }
 
 #[get("/image/deliver")]
